8b44a1970b7105a7d7ae6e1caa72b3c97efcfcad
nikita@aveldent.ru <Ni5020830>
2022-12-09 10:56:39 +0300

    Админка, начало верстки с частичным использованием бутстрапа

--- resources/views/layouts/mainAdmin.blade.php
+++ resources/views/layouts/mainAdmin.blade.php
@@ -6,11 +6,53 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
+
     <title>Document</title>
 </head>
 <body>
-<div class="container">
+
+<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">Company name</a>
+</header>
+
+<div class="container-fluid">
+    <div class="row">
+        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar ">
+            <div class="position-sticky pt-3">
+                <ul class="nav flex-column">
+                    <li class="nav-item">
+                        <a class="nav-link active"  href="#">
+                            Главная
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            Добавление темы / Изменение темы
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            Добавление тестов / Изменение тестов
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                           Добавление практики / Изменение практики
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            Ученики
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
     @yield('content')
+
+    </div>
 </div>
+
 </body>
 </html>
