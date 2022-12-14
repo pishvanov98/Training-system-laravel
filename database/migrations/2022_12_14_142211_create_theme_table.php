37c9d182e687d28e9c888c1d3ca5dc4d434831e8
nikita@aveldent.ru <Ni5020830>
2022-12-14 17:31:43 +0300

    Админка создал миграцию

---
+++ database/migrations/2022_12_14_142211_create_theme_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThemeTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('theme', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_theme');
+            $table->string('category');
+            $table->text('small_description');
+            $table->text('full_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('theme');
+    }
+}
