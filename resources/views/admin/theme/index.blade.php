5b50af64a47a20c4d253e33054464e8d1d68803c
nikita@aveldent.ru <Ni5020830>
2022-12-15 15:33:47 +0300

    Админка вывод в шаблоне

--- resources/views/admin/theme/index.blade.php
+++ resources/views/admin/theme/index.blade.php
@@ -3,12 +3,16 @@
     <div class="main_content_admin">
         <h2> Страница  с темами</h2>
         @foreach($themes as $theme)
-            <div>
+            <div class="container_theme">
                 <a href="#">
                     <img  src=" {{ asset($theme->image) }} "  >
                     <span> {{ $theme->name_theme }} </span>
                     <span> {{ $theme->small_description }} </span>
                 </a>
+                <div class="button_edit_theme">
+                    <a href="#"> Изменить</a>
+                    <a href="#"> Удалить</a>
+                </div>
             </div>
         @endforeach
 
