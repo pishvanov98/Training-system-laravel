ff078d6c6c602c0b6460a0dc52d10ff7dd88b96a
nikita@aveldent.ru <Ni5020830>
2022-12-16 14:58:21 +0300

    Админка вывод в шаблоне

--- resources/views/admin/theme/index.blade.php
+++ resources/views/admin/theme/index.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.mainAdmin')
 @section('content')
     <div class="main_content_admin">
-        <h2> Страница  с темами</h2>
+        <div class="top_block"><h2> Страница  с темами</h2><a href="#">Добавить тему</a></div>
         @foreach($themes as $theme)
             <div class="container_theme">
                 <a href="#">
                     <img  src=" {{ asset($theme->image) }} "  >
-                    <span> {{ $theme->name_theme }} </span>
-                    <span> {{ $theme->small_description }} </span>
+                    <span class="name_theme"> {{ $theme->name_theme }} </span>
+                    <span class="description_theme"> {{ $theme->small_description }} </span>
                 </a>
                 <div class="button_edit_theme">
                     <a href="#"> Изменить</a>
