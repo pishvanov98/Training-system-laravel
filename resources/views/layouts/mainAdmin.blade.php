9acf728ec2be9ad922e74fd1dfa2cd0d28ebc57a
nikita@aveldent.ru <Ni5020830>
2023-01-19 11:40:59 +0300

    jQuery

--- resources/views/layouts/mainAdmin.blade.php
+++ resources/views/layouts/mainAdmin.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href=" {{ asset('css/app.css') }} ">
-
+    <script src="{{asset('js/jquery-3.6.3.min.js')}}"></script>
     <title>Админ панель</title>
 </head>
 <body>
