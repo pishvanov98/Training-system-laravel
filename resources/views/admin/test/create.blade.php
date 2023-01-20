e7924c1c7ebaaff7e1ca8fe42cb20a511f7176c6
nikita@aveldent.ru <Ni5020830>
2023-01-20 10:59:11 +0300

    Тесты js добавление тестов и вопросов

--- resources/views/admin/test/create.blade.php
+++ resources/views/admin/test/create.blade.php
@@ -9,19 +9,19 @@
 {{--            шаблон вопроса--}}
             <div class="block_test hide">
                 <div class="mb-3">
-                    <label for="exampleInputquestion" class="form-label num_question"></label>
-                    <input type="text" name="question" class="form-control" id="exampleInputquestion" >
+                    <label  class="form-label num_question"></label>
+                    <input type="text" name="question" class="form-control input_question"  >
 
                 </div>
 
-                <div class="answer_create_block">
+                <div data-num_ansver_item="" class="answer_create_block">
 
-                    <div class="mb-3 answer_create_item">
-                        <label for="exampleInputanswer" class="form-label num_answer"></label>
-                        <input type="text" name="answer" class="form-control" id="exampleInputanswer" >
+                    <div class="mb-3 answer_create_item ">
+                        <label  class="form-label num_answer"></label>
+                        <input type="text" name="answer" class="form-control input_answer"  >
                         <div class="form-check">
-                            <input class="form-check-input" name="correct_answer" type="checkbox" value="" id="correct_answer">
-                            <label class="form-check-label" for="correct_answer">
+                            <input class="form-check-input correct_input_answer" name="correct_answer" type="checkbox" value="" >
+                            <label class="form-check-label" >
                                 Правильный ответ
                             </label>
                         </div>
@@ -35,11 +35,11 @@
 
             {{--            шаблон ответа--}}
             <div class="mb-3 answer_create_item hide">
-                <label for="exampleInputanswer" class="form-label num_answer">Введите вариант ответа</label>
-                <input type="text" name="answer" class="form-control" id="exampleInputanswer" >
+                <label  class="form-label num_answer">Введите вариант ответа</label>
+                <input type="text" name="answer" class="form-control input_answer"  >
                 <div class="form-check">
-                    <input class="form-check-input" name="correct_answer" type="checkbox" value="" id="correct_answer">
-                    <label class="form-check-label" for="correct_answer">
+                    <input class="form-check-input correct_input_answer" name="correct_answer" type="checkbox" value="" >
+                    <label class="form-check-label" >
                         Правильный ответ
                     </label>
                 </div>
@@ -53,7 +53,7 @@
         </div>
         <div class="block_test_bottom">
                     <button type="submit" class="btn btn-primary add_question" onclick="add_question();">Добавить вопрос</button>
-                    <button type="submit" class="btn btn-primary save_test">Сохранить тест</button>
+                    <button type="submit" onclick="create_test();" class="btn btn-primary save_test">Сохранить тест</button>
         </div>
     </div>
 
@@ -70,7 +70,6 @@
            $(".block_test.hide").clone().appendTo($(".block_test_wrapper"));
 
            $('.block_test:last-child').removeClass('hide');
-
            $('.block_test:last-child .num_question').text("Введите вопрос №"+num);
             // $('.block_test:last-child ').addClass("block_test_num"+question);
             $('.block_test:last-child .answer_create_block').addClass("add_answer_num"+num);
@@ -81,11 +80,36 @@
        }
 
            function add_answer(item_answer){
-               console.log();
                $(".answer_create_item.hide").clone().appendTo($("."+item_answer));
-               $(".answer_create_block .answer_create_item:last-child .num_answer").text("Введите вариант ответа №"+num_answer);
-               $(".answer_create_block .answer_create_item:last-child").removeClass('hide');
+
+               if($("."+item_answer).attr("data-num_ansver_item")){
+                   num_answer = $("."+item_answer).attr("data-num_ansver_item");
+               }
+
+               $("."+item_answer+ "  .answer_create_item:last-child .num_answer").text("Введите вариант ответа №"+num_answer);
+               $("."+item_answer+ "  .answer_create_item:last-child").removeClass('hide');
                num_answer++;
+               $("."+item_answer).attr("data-num_ansver_item", num_answer);
+           }
+
+
+           function create_test(){
+
+               $('.block_test').each(function(i, obj) {
+                   if(!$(obj).hasClass('hide')){ //получаю вопрос перебором
+                   var question=$(obj).find('.input_question').val();
+                   console.log(question);
+
+                       $(obj).find('.answer_create_item').each(function(i, obj2) {
+                           if(!$(obj2).hasClass('hide')){ //получаю ответ перебором
+                               var answer=$(obj2).find('.input_answer').val();
+                               console.log(answer);
+                           }
+                       });
+
+                   }
+               });
+
            }
 
     </script>
