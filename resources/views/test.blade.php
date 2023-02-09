1293a4daa8c31df86615945b88fd2a8e00043842
nikita@aveldent.ru <Ni5020830>
2023-02-09 17:27:22 +0300

    Вывод теста, слайдер

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -3,30 +3,34 @@
 
     @if($mass_test)
         <script src="{{asset('js/owl.carousel.min.js')}}"></script>
-        <div class="owl-carousel owl-theme">
-            @php $i=1; @endphp
-@foreach($mass_test as $key=> $item_mass)
-                <div class="wrapper_test_item">
-                    <div class="num_test"> {{$i.'/'.$count_mass_test}} </div>
-                    <div class="text_quantity"> {{$key}} </div>
-                    <div class="quantity_test">
-                        @foreach($item_mass as $key=> $item)
+       <div class="wrapper_test_slider">
+           <div class="owl-carousel owl-theme">
+               @php $i=1; @endphp
+               @foreach($mass_test as $key=> $item_mass)
+                   <div class="wrapper_test_item">
+                       <div class="num_test"> {{$i.'/'.$count_mass_test}} </div>
+                       <div class="text_quantity"> {{$key}} </div>
+                       <div class="quantity_test">
+                           @foreach($item_mass as $key=> $item)
 
-                            <div>
-                                <input type="radio" id="contactChoice{{$key}}"
-                                       name="test{{$i}}" value="{{$item[1]}}">
-                                <label for="contactChoice{{$key}}">{{$item[0]}}</label>
-                            </div>
-                        @endforeach
-                    </div>
-                    @php $i++; @endphp
-                </div>
-@endforeach
-        </div>
-        <div class="button_hide_test">
-            <div class="go-prev">назад</div>
-            <div class="go-next">вперед</div>
-        </div>
+                               <div>
+                                   <input type="radio" id="contactChoice{{$key}}"
+                                          name="test{{$i}}" value="{{$item[1]}}">
+                                   <label for="contactChoice{{$key}}">{{$item[0]}}</label>
+                               </div>
+                           @endforeach
+                       </div>
+                       @php $i++; @endphp
+                   </div>
+               @endforeach
+           </div>
+           <div class="button_hide_test">
+               <div class="go-prev">назад</div>
+               <div class="go-next">вперед</div>
+           </div>
+
+       </div>
+
         <script>
             $(document).ready(function() {
 
@@ -34,7 +38,7 @@
 
                 owl.owlCarousel({
                     loop: true,
-                    margin: 10,
+                    // margin: 10,
                     nav: false,
                     dots: false,
                     responsive: {
