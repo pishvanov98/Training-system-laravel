40cc72ee6de42e21ccb00c2f1c86c08fa122e190
nikita@aveldent.ru <Ni5020830>
2023-02-22 13:00:41 +0300

    Миграция таблицы с ответами на тесты

---
+++ database/migrations/2023_02_21_103857_create_answer_test_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnswerTestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answer_test', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->integer('id_test');
+            $table->text('answer_test');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answer_test');
+    }
+}
