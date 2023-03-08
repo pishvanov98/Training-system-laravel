24c1b46321a015109da0de70fbce1ed066f63a05
nikita@aveldent.ru <nikita@aveldent.ru>
2023-03-08 20:41:02 +0300

    ЛК пользователя

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -1,13 +1,80 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\AdminTest;
+use App\Models\AdminTestAnswer;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller{
 
     public function index (){
 
-        return view('account');
+        if(!empty(auth()->id())){
+
+            $data = AdminTestAnswer::where('id_user', '=', auth()->id())->get();
+
+            $data = $data->toArray();
+            $sum=0;
+            foreach ($data as $item){
+                $expl= explode('/',$item['answer_count']);//правильно отмеченых
+                if(empty($sum)){
+                    $sum = (int)$expl[0];
+                }else{
+                    $sum = $sum + (int)$expl[0];
+                }
+
+            }
+            $tests= AdminTest::all();
+            $tests->toArray();
+            $count_mass_test=0;
+            foreach ($tests as $test){
+
+                $json_decode_test=json_decode($test, true);
+
+
+
+                $json_decode_test['test_info']=explode(',',$json_decode_test['test_info']);
+
+                $json_decode_test['test_info'] = str_replace("]", "", $json_decode_test['test_info']);
+                $json_decode_test['test_info'] = str_replace("[", "", $json_decode_test['test_info']);
+                $json_decode_test['test_info'] = str_replace('"', "", $json_decode_test['test_info']);
+
+
+
+
+                $mass_test=array();
+                $quantity='';
+                foreach ($json_decode_test['test_info'] as $item){ // получил массив с вопросом и ответами
+
+
+                    if (!str_contains($item, '|')) {
+                        $quantity=$item;
+                        continue;
+                    }else{
+                        $item=explode('|', $item);
+                    }
+
+
+                    $mass_test[$quantity][]=$item;
+
+                }
+                if(empty($count_mass_test)){
+                    $count_mass_test=count($mass_test);
+                }else{
+                    $count_mass_test=$count_mass_test + count($mass_test);
+                }
+            }
+
+            $percentage = $sum;
+            $totalWidth = $count_mass_test;
+
+            $new_width = ($percentage/$totalWidth) * 100;
+
+
+            return view('account', compact('sum', 'count_mass_test','new_width'));
+        }else{
+            return redirect()->route('home');
+        }
 
     }
 
--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,6 +1,10 @@
 @extends('layouts.app')
 @section('content')
 
+    <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="{{$sum}}" aria-valuemin="0" aria-valuemax="{{$count_mass_test}}">
+        <div class="progress-bar" style="width: {{$new_width}}%">{{$sum}} из {{$count_mass_test}}</div>
+    </div>
+
     <span>Тут личный кабинет, тут будет информация о пользователе, строка прогресса и возможность перейти по тестам и пересдать их.</span>
 
 
